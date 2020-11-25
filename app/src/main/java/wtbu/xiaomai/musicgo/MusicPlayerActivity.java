f874ff18007502a8437440f5e6eefeb3c666f611
Kingsmai <xsbugh@gmail.com>
2020-11-25 13:44:49 +0800

    https://www.bilibili.com/video/BV1KV411Y7EP/ 视频教学源码更新

--- app/src/main/java/wtbu/xiaomai/musicgo/MusicPlayerActivity.java
+++ app/src/main/java/wtbu/xiaomai/musicgo/MusicPlayerActivity.java
@@ -11,17 +11,20 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MusicPlayerActivity extends AppCompatActivity {
-
     private TextView songName, singerName;
     private ImageView diskImage;
+    private TextView lyricPrev, lyricCurrent, lyricNext;
+    private SeekBar musicProgress;
+    private TextView currentTime, totalTime;
     private ImageButton prevBtn, playPauseBtn, nextBtn;
-    private boolean isPausing = false;
-    private boolean isPlaying = false;
     private ObjectAnimator animator;
+
+    private boolean isPausing, isPlaying; // 音乐暂停状态，音乐第一次播放之后变为true
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,53 +37,63 @@ public class MusicPlayerActivity extends AppCompatActivity {
         songName = findViewById(R.id.tv_song_name);
         singerName = findViewById(R.id.tv_song_singer);
         diskImage = findViewById(R.id.iv_disk);
+        lyricPrev = findViewById(R.id.tv_lyric_previous);
+        lyricCurrent = findViewById(R.id.tv_lyric_current);
+        lyricNext = findViewById(R.id.tv_lyric_next);
+        musicProgress = findViewById(R.id.sb_progress);
+        currentTime = findViewById(R.id.tv_progress_current);
+        totalTime = findViewById(R.id.tv_progress_total);
         prevBtn = findViewById(R.id.btn_prev);
         playPauseBtn = findViewById(R.id.btn_play_pause);
         nextBtn = findViewById(R.id.btn_next);
 
-        OnClick onClick = new OnClick();
+        OnClickControl onClick = new OnClickControl();
         prevBtn.setOnClickListener(onClick);
         playPauseBtn.setOnClickListener(onClick);
         nextBtn.setOnClickListener(onClick);
 
-        animator = ObjectAnimator.ofFloat(diskImage, "rotation", 0, 360F); // 初始化状态
-        animator.setDuration(10000); // 转动时长,10秒
-        animator.setInterpolator(new LinearInterpolator()); // 转动的时间函数,线性转动
+        animator = ObjectAnimator.ofFloat(diskImage, "rotation", 0, 360.0F); // 初始化状态
+        animator.setDuration(10000); // 转动时长，10秒
+        animator.setInterpolator(new LinearInterpolator()); // 时间函数，有很多类型
         animator.setRepeatCount(-1); // 一直旋转
     }
 
-    private class OnClick implements View.OnClickListener {
+    private class OnClickControl implements View.OnClickListener {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.btn_prev:
-                    Log.i("INFO", "onClick: Previous button clicked!");
                     // 重播
-                    animator.start(); // 图片重新转动
+                    Log.i("INFO", "onClick: 重播按钮被点击！");
+                    playPauseBtn.setImageResource(R.drawable.control_pause_dark); // 切换成暂停键
+                    animator.start();
                     break;
                 case R.id.btn_play_pause:
-                    Log.i("INFO", "onClick: Play pause button clicked!");
-                    if (!isPausing && !isPlaying) {
+                    // 开始暂停
+                    Log.i("INFO", "onClick: 开始暂停按钮被点击！");
+                    if (!isPausing && !isPlaying) { // 暂停状态，且从未被播放
                         // 开始播放
-                        playPauseBtn.setImageResource(R.drawable.control_pause_dark);
+                        playPauseBtn.setImageResource(R.drawable.control_pause_dark); // 切换成暂停键
                         animator.start();
-                    } else if (!isPausing && isPlaying) {
+                        isPlaying = true;
+                    } else if (!isPausing && isPlaying) { // 暂停状态，且被播放过一次
                         // 继续播放
-                        playPauseBtn.setImageResource(R.drawable.control_pause_dark);
+                        playPauseBtn.setImageResource(R.drawable.control_pause_dark); // 切换成暂停键
                         animator.resume();
-                    } else {
+                    } else { // 播放状态
                         // 暂停播放
-                        playPauseBtn.setImageResource(R.drawable.control_play_dark);
+                        playPauseBtn.setImageResource(R.drawable.control_play_dark); // 切换成播放键
                         animator.pause();
                     }
-                    isPausing = !isPausing; // 取反
-                    isPlaying = true; // 表示歌曲已经开始播放
+                    isPausing = !isPausing; // 切换状态
                     break;
                 case R.id.btn_next:
                     // 切歌
-                    Log.i("INFO", "onClick: Next button clicked!");
+                    Log.i("INFO", "onClick: 切歌按钮被点击！");
                     break;
                 default:
+                    // 有Bug了
+                    Log.i("INFO", "onClick: 按钮点击了，但是有BUG！");
             }
         }
     }
