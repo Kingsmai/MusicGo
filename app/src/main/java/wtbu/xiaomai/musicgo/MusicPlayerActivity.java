695dad801073ba0b4bd5c3157164a3b834fae749
Kingsmai <xsbugh@gmail.com>
2020-11-27 20:37:14 +0800

    https://www.bilibili.com/video/BV1Cy4y1q7vR/

--- app/src/main/java/wtbu/xiaomai/musicgo/MusicPlayerActivity.java
+++ app/src/main/java/wtbu/xiaomai/musicgo/MusicPlayerActivity.java
@@ -3,17 +3,20 @@ package wtbu.xiaomai.musicgo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MusicPlayerActivity extends AppCompatActivity {
     private TextView songName, singerName;
@@ -22,15 +25,30 @@ public class MusicPlayerActivity extends AppCompatActivity {
     private SeekBar musicProgress;
     private TextView currentTime, totalTime;
     private ImageButton prevBtn, playPauseBtn, nextBtn;
-    private ObjectAnimator animator;
 
-    private boolean isPausing, isPlaying; // 音乐暂停状态，音乐第一次播放之后变为true
+    private ObjectAnimator animator;
+    private MediaPlayer player;
+
+    private int currentPlaying = 0; // 用作 ArrayList 下标，当前播放的歌曲
+    private ArrayList<Integer> playList = new ArrayList<>();
+
+    private boolean isPausing = false, isPlaying = false; // 音乐暂停状态，音乐第一次播放之后变为true
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
         init();
+        preparePlaylist();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                if (isPlaying) {
+                    updateTimer();
+                }
+            }
+        };
+        new Timer().scheduleAtFixedRate(timerTask, 0, 500);
     }
 
     void init() {
@@ -52,10 +70,54 @@ public class MusicPlayerActivity extends AppCompatActivity {
         playPauseBtn.setOnClickListener(onClick);
         nextBtn.setOnClickListener(onClick);
 
+        OnSeekBarChangeControl onSbChange = new OnSeekBarChangeControl();
+        musicProgress.setOnSeekBarChangeListener(onSbChange);
+
         animator = ObjectAnimator.ofFloat(diskImage, "rotation", 0, 360.0F); // 初始化状态
         animator.setDuration(10000); // 转动时长，10秒
         animator.setInterpolator(new LinearInterpolator()); // 时间函数，有很多类型
         animator.setRepeatCount(-1); // 一直旋转
+    }
+
+    private void preparePlaylist() {
+        Field[] fields = R.raw.class.getFields();
+        for (int count = 0; count < fields.length; count++) {
+            Log.i("Raw Asset", fields[count].getName());
+            try {
+                int resId = fields[count].getInt(fields[count]);
+                playList.add(resId);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void prepareMedia() {
+        if (isPlaying) {
+            player.stop();
+            player.reset();
+        }
+        player = MediaPlayer.create(getApplicationContext(), playList.get(currentPlaying));
+        int musicDuration = player.getDuration();
+        musicProgress.setMax(musicDuration);
+        int sec = musicDuration / 1000;
+        int min = sec / 60;
+        sec -= min * 60;
+        String musicTime = String.format("%02d:%02d", min, sec);
+        totalTime.setText(musicTime);
+        player.start();
+    }
+
+    private void updateTimer() {
+        runOnUiThread(() -> {
+            int currentMs = player.getCurrentPosition();
+            int sec = currentMs / 1000;
+            int min = sec / 60;
+            sec -= min * 60;
+            String time = String.format("%02d:%02d", min, sec);
+            musicProgress.setProgress(currentMs);
+            currentTime.setText(time);
+        });
     }
 
     private class OnClickControl implements View.OnClickListener {
@@ -63,10 +125,16 @@ public class MusicPlayerActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch (v.getId()) {
                 case R.id.btn_prev:
-                    // 重播
+                    // 重播、上切
                     Log.i("INFO", "onClick: 重播按钮被点击！");
                     playPauseBtn.setImageResource(R.drawable.control_pause_dark); // 切换成暂停键
                     animator.start();
+                    if (!player.isPlaying()) {
+                        currentPlaying = --currentPlaying % playList.size();
+                    }
+                    prepareMedia();
+                    isPausing = false;
+                    isPlaying = true;
                     break;
                 case R.id.btn_play_pause:
                     // 开始暂停
@@ -75,25 +143,59 @@ public class MusicPlayerActivity extends AppCompatActivity {
                         // 开始播放
                         playPauseBtn.setImageResource(R.drawable.control_pause_dark); // 切换成暂停键
                         animator.start();
+                        prepareMedia();
                         isPlaying = true;
-                    } else if (!isPausing && isPlaying) { // 暂停状态，且被播放过一次
+                    } else if (isPausing && isPlaying) { // 暂停状态，且被播放过一次
                         // 继续播放
                         playPauseBtn.setImageResource(R.drawable.control_pause_dark); // 切换成暂停键
                         animator.resume();
+                        player.start();
                     } else { // 播放状态
                         // 暂停播放
                         playPauseBtn.setImageResource(R.drawable.control_play_dark); // 切换成播放键
                         animator.pause();
+                        player.pause();
                     }
                     isPausing = !isPausing; // 切换状态
                     break;
                 case R.id.btn_next:
                     // 切歌
                     Log.i("INFO", "onClick: 切歌按钮被点击！");
+                    playPauseBtn.setImageResource(R.drawable.control_pause_dark); // 切换成暂停键
+                    currentPlaying = ++currentPlaying % playList.size();
+                    prepareMedia();
+                    animator.start();
+                    isPausing = false;
+                    isPlaying = true;
                     break;
                 default:
                     // 有Bug了
                     Log.i("INFO", "onClick: 按钮点击了，但是有BUG！");
+            }
+        }
+    }
+
+    private class OnSeekBarChangeControl implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            if (fromUser) {
+                player.seekTo(progress);
+            }
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            player.pause();
+            animator.pause();
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            player.start();
+            if (seekBar.getProgress() < 10) {
+                animator.start();
+            } else {
+                animator.resume();
             }
         }
     }
